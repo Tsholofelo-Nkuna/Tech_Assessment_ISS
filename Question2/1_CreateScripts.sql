8a0de9449a18c8adc638cfa9625e3dbec3bd1f0e
Tsholofelo Nkuna <tnkuna@hartic.com>
2024-11-21 19:38:09 +0200

    Question2

---
+++ Question2/1_CreateScripts.sql
@@ -0,0 +1,52 @@
+﻿Create Table Facilitators (
+ Id bigint Primary Key Identity(1,1),
+ FirstName nvarchar(128),
+ LastName nvarchar(128),
+ Email nvarchar(128),
+ Phone nvarchar(128)
+);
+
+create Table Clients (
+ [Name] nvarchar(128),
+ [Id] bigint Primary Key Identity(1,1),
+ ClientType int, --indicates type of client (business or individual)
+ Email nvarchar(128),
+ Phone nvarchar(128)
+)
+
+Create Table Students (
+ Id bigint Primary Key Identity(1,1),
+ FirstName nvarchar(128),
+ LastName nvarchar(128), 
+ Email nvarchar(128),
+ Phone nvarchar(128),
+ ClientId bigint references Clients(Id), 
+)
+
+Create Table Courses (
+Id bigint Primary Key Identity(1,1),
+MaxStudentIntake int, 
+[Name] nvarchar(128), 
+[Description] nvarchar(max),
+FacilitatorId bigint Foreign Key References Facilitators(Id),
+StartDate DateTime,
+EndDate DateTime
+);
+
+Create Table X_StudentCourses(
+  StudentId bigint Foreign Key References Students(Id),
+  CourseId bigint Foreign Key References Courses(Id)
+)
+
+Create Table X_StudentCourseResults(
+ StudendId bigint Foreign Key References Students(Id),
+ CourseId bigint Foreign Key References Courses(Id),
+ Competent bit not null default(0)
+)
+
+Create Table CourseCertificate(
+  DateIssued DateTime,
+  ExpirationDate DateTime,
+  IssuedTo bigint Foreign Key References Students(Id),
+  CourseId bigint Foreign Key References Courses(Id)
+)
